c88bed9a6bb159ccdfb0fbbadc77842e56730e1b
Mohamed Waseem <113375456+Mdwaseem2002@users.noreply.github.com>
2025-01-16 16:11:34 +0530

    Update firebase.js

--- src/app/components/firebase.js
+++ src/app/components/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxvDPMWivyt65pzXU4OimCTHrLX0O0-ws",
-    authDomain: "ecommerce-c72af.firebaseapp.com",
-    projectId: "ecommerce-c72af",
-    storageBucket: "ecommerce-c72af.firebasestorage.app",
-    messagingSenderId: "895719588422",
-    appId: "1:895719588422:web:40f0eaf0c41226b5520047",
-    measurementId: "G-X4VRBYPM1S"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
